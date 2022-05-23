0f7dec9dca633e7fa727d1d897f6f8f6c2fee799
zubayer <zubayer3570@gmail.com>
2022-05-24 03:10:25 +0600

    'hi'

--- index.js
+++ index.js
@@ -30,13 +30,44 @@ const client = new MongoClient(uri)
 const run = () => {
     try {
         client.connect()
-        // const collection1 = client.db().collection()
-        // const collection2 = client.db().collection()
-        // const collection3 = client.db().collection()
+        const collection1 = client.db('manufacture').collection('tools')
+        const collection2 = client.db('manufacture').collection('users')
+        const collection3 = client.db('manufacture').collection('orders')
         // app.get('/', async (req, res) => { })
         // app.post('/', async (req, res) => { })
         // app.delete('/', async (req, res) => { })
         // app.put('/', async (req, res) => { })
+        app.post('/addUser', async (req, res) => {
+            const email = req.body.email
+            await collection2.insertOne({ email })
+            res.send({ message: 'user added' })
+        })
+        app.post('/addTools', async (req, res) => {
+            const tools = req.body.tools
+            await collection1.insertOne(tools)
+            res.send({ message: 'tools added' })
+        })
+        app.post('/checkAdmin', async (req, res) => {
+            const email = req.body.email
+            const result = await collection2.findOne({ email })
+            if (result?.admin) {
+                res.send({ message: 'User is an admin' })
+            }
+        })
+        app.get('/tools', async (req, res) => {
+            const tools = await collection1.find({}).toArray()
+            res.send(tools)
+        })
+        app.get('/toolDetails/:id', async (req, res) => {
+            const { id } = req.params
+            const toolDetails = await collection1.findOne({ _id: ObjectId(id) })
+            res.send(toolDetails)
+        })
+        app.post('/placeOrder', async (req, res) => {
+            const order = req.body.order
+            await collection3.insertOne(order)
+            res.send({ message: 'hi' })
+        })
     } finally { }
 }
 run()
